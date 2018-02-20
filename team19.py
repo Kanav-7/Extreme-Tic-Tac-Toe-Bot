3a4f8f1e9815eabf3517e63b4b34b26c4c23e4ce
Kanav Gupta <kanavgupta0711@gmail.com>
2018-02-20 22:54:12 +0530

    Initaial Structure

---
+++ team19.py
@@ -0,0 +1,12 @@
+import random
+
+class Team19:
+	def __init__(self):
+		# self.board = board
+		pass
+
+	def move(self, board, old_move, flag):
+		#You have to implement the move function with the same signature as this
+		#Find the list of valid cells allowed
+		cells = board.find_valid_move_cells(old_move)
+		return cells[random.randrange(len(cells))]
